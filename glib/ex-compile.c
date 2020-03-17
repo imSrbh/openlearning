90d1ecc98d269e6f4fdd38187615e60c10a713d6
imsrbh <imsaurabh26@gmail.com>
2020-03-17 07:41:37 +0530

    update

--- glib/ex-compile.c
+++ glib/ex-compile.c
@@ -1,8 +1,8 @@
-//ex‑compile.c
-#include <glib.h>
-int main(int argc, char∗∗ argv) {
- GList∗ list = NULL;
- list = g_list_append(list, "Hello world!");
- printf("The first item is '%s'\n", g_list_first(list)‑>data);
- return 0;
+#include<glib.h>
+int main(int argc, char** argv){
+	Glist* list =NULL;
+	list=g_list_append(list, "Hello world!!");
+	printf("The first item is '%s'\n", g_list_firt(list)->data);
+	return 0;
 }
+
