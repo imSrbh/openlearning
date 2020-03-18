280226bc7bdc0e6e9999b675c6545e1006e5d677
imsrbh <imsaurabh26@gmail.com>
2020-03-18 18:37:29 +0530

    Some Bugs

--- GArray/test2.c
+++ GArray/test2.c
@@ -1,27 +1,32 @@
-#include<glib.h>
+//cc test2.c `pkg-config  --cflags --libs glib-2.0` -o noseg
 
+
+#include<glib.h>
 int main(int argc, char** argv){
 	GArray* ar = g_array_new(FALSE, FALSE, sizeof(char*));
-	//char* a = "Hii", *b =  "Saurabh", *c = "Singh";
-	char* a;
-	char* b, c;
+	char* a = "Hii", *b =  "Saurabh", *c = "Singh";
+	//char* a, b, c;
+	//char* b;
 	//GArray* arr = g_array_new(FALSE, FALSE, sizeof(int*));
 	//int* x=2, *y=3;
 	//g_array_append_val(arr, x);
 	//g_array_append_val(ar, y);
-        g_print("Now we'll enter element from cmd line:\n");
-	scanf("%s", &a);
-	scanf("%s", &b);
-	scanf("%s", &c);
+        //g_print("Now we'll enter element from cmd line:\n");
+	//scanf("%s", &a);
+	//scanf("%s", &b);
+	//scanf("%s", &c);
 	g_array_append_val(ar, a);
 	g_array_append_val(ar, b);
 	g_array_append_val(ar, c);
-	int len;
-	len=(ar->len);
-	g_print("len: %d\n",len);
-	g_print("Total no. of items in the array : %d\n", ar->len);
+	 printf("There are now %d items in the array\n", ar->len);
+	 printf("The first item is '%s'\n", g_array_index(ar, char*, 0));
+	 printf("The third item is '%s'\n", g_array_index(ar, char*, 2));
 
-
+	//printf("The first item is '%s'\n", g_array_index(ar, char*, 1));
+	//int len;
+	//len=(ar->len);
+	//g_print("len: %d\n",len);
+	//g_print("Total no. of items in the array : %d\n", ar->len);
 	//g_array_remove_index(ar, 1);
 	//g_print("NOW Total no. of items in the array : %d\n", ar->len);
 	//printf("INT array Total no. of items in the array : %d\n", arr->len);
